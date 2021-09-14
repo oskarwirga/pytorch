8a973bb4918d58fd50d0418d83d6f8dd65977367
soulitzer <soulitzer@gmail.com>
2021-09-14 13:47:42 -0400

    Add InplaceOrView boxed fallback
    ghstack-source-id: 8753131515f2b500c1b1fd2549cc2cf089a4de53
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63878

--- torch/csrc/autograd/autograd_not_implemented_fallback.h
+++ torch/csrc/autograd/autograd_not_implemented_fallback.h
@@ -8,4 +8,15 @@ namespace autograd {
 
 TORCH_API torch::CppFunction autogradNotImplementedFallback();
 
+TORCH_API torch::CppFunction autogradNotImplementedInplaceOrViewFallback();
+
+
+#define REGISTER_AUTOGRAD_NOT_IMPLEMENTED_FALLBACK(ns, op)       \
+  TORCH_LIBRARY_IMPL(ns, Autograd, m) {                          \
+    m.impl(op, autogradNotImplementedFallback());                \
+  }                                                              \
+  TORCH_LIBRARY_IMPL(ns, ADInplaceOrView, m) {                   \
+    m.impl(op, autogradNotImplementedInplaceOrViewFallback());   \
+  }
+
 }} // namespace torch::autograd
